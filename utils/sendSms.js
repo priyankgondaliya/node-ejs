28e68202cd2bd90b580fc2ef9c2facb58a54705f
Pranav <pranav@apptodate.co.il>
2024-08-01 14:07:52 +0530

    twilio

--- utils/sendSms.js
+++ utils/sendSms.js
@@ -1,12 +1,15 @@
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = require("twilio")(accountSid, authToken);
+const client = require("twilio")(
+  "AC890d31575db260908bd9d0d0b792f296",
+  "76a187da26b6d365fba440a42db89277"
+);
 console.log(client, "client");
 
 const sendOtp = async function (to, otp) {
   return client.messages.create({
     body: `Your OTP is ${otp}. OTP is valid for 5 minutes.`,
-    from: process.env.TWILIO_NUMBER,
+    from: +15753080052,
     to,
   });
 };
